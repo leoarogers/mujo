30507d5ed1ef70c733b60a6c8246df53a2f48185
Leo Rogers <leo.a.rogers@gmail.com>
2015-09-20 16:54:18 -0400

    test

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Mujo
   class Application < Rails::Application
-    config.serve_static_assets = true
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.paths << Rails.root.join('app/assets/fonts')
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
