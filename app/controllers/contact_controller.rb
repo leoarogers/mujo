274c5468f3c16ba5bdfc1e460baa79e7e6c673d3
Leo Rogers <leo.a.rogers@gmail.com>
2015-10-15 02:09:22 -0400

    test

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,7 +6,7 @@ class ContactController < ApplicationController
 	def create
 		contact = Contact.find_or_create_by(email: params['message']['contact_email'])
 		message = Message.create(contact_id: contact.id, content: params['message']['message_content'])
-		session[:message] = "Thank you for your feedback. We'll be in touch."
+		session[:message] = "We'll be in touch."
 		redirect_to contact_index_path
 	end
 
