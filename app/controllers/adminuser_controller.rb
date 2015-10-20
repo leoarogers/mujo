a500db4d3ed6f774c0e0acbfe417a439aa9c06fc
Leo Rogers <leo.a.rogers@gmail.com>
2015-10-19 23:50:13 -0400

    admin page

--- app/controllers/adminuser_controller.rb
+++ app/controllers/adminuser_controller.rb
@@ -2,7 +2,9 @@ class AdminuserController < ApplicationController
 
 	def index
 		@users = User.all
+		@user_count = @users.count
 		@contacts = Contact.all
+		@message_count = @contacts.count
 	end
 
 end
