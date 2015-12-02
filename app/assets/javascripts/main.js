0afb4c9bda7ca1ffc9c8e632a42248d7f247ae1c
Leo Rogers <leo.a.rogers@gmail.com>
2015-12-02 01:16:31 -0500

    test

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	$('.down_arrow').fadeIn(2500);
 
 	// set height of shop images
-	var height = window.innerHeight ? window.innerHeight - 70 + 120 : $(window).height() - 70;
+	var height = window.innerHeight ? window.innerHeight - 70 + 150 : $(window).height() - 70;
 	// var height = $(window).height() - 70;
 	$("div.active").css('height', height+'px');
 
