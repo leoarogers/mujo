f5c9f29edabbdbfb35d08793dab2a8c442595ef7
Leo Rogers <leo.a.rogers@gmail.com>
2016-01-11 22:09:33 -0500

    make contact form include contact name

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,6 +5,7 @@ class ContactController < ApplicationController
 
 	def create
 		contact = Contact.find_or_create_by(email: params['message']['contact_email'])
+		contact.update(name: params['message']['contact_name'])
 		message = Message.create(contact_id: contact.id, content: params['message']['message_content'])
 		session[:message] = "We'll be in touch."
 		redirect_to contact_index_path
