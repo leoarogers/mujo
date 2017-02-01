dc3bdf70f9134d7728779cca9e82e8537807d734
Leo Rogers <leo.a.rogers@gmail.com>
2017-01-31 21:22:57 -0500

    test

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -59,193 +59,193 @@ $(document).ready(function() {
 	// Lookbook Scroll Logic
 	//-------------------------------------
 
-	// LOOKBOOK.forEach((lookbook) => {
-	// 	var lookbook_pic_number = 1;
+	LOOKBOOK.forEach(function(lookbook) {
+		var lookbook_pic_number = 1;
 
-	// 	$(".lookbook" + lookbook.number + "_pic").click(function(e) {
-	// 		var $target = e.target;
+		$(".lookbook" + lookbook.number + "_pic").click(function(e) {
+			var $target = e.target;
 
-	// 		if ($target.className == "lb" + lookbook.number + "_right_arrow" || $target.className == "lb" + lookbook.number + "_left_arrow") return;
+			if ($target.className == "lb" + lookbook.number + "_right_arrow" || $target.className == "lb" + lookbook.number + "_left_arrow") return;
 
-	// 		if (lookbook_pic_number < lookbook.length) {
-	// 			lookbook_pic_number += 1;
-	// 			$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
-	// 			$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
-	// 		} else {
-	// 			lookbook_pic_number = 1;
-	// 			$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
-	// 			$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
-	// 		};
-	// 	});
+			if (lookbook_pic_number < lookbook.length) {
+				lookbook_pic_number += 1;
+				$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
+				$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
+			} else {
+				lookbook_pic_number = 1;
+				$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
+				$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
+			};
+		});
 
-	// 	$(".lb" + lookbook.number + "_right_arrow").click(function(e) {
-	// 		if (lookbook_pic_number < lookbook.length) {
-	// 			lookbook_pic_number += 1;
-	// 			$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
-	// 			$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
-	// 		} else {
-	// 			lookbook_pic_number = 1;
-	// 			$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
-	// 			$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
-	// 		};
-	// 	});
+		$(".lb" + lookbook.number + "_right_arrow").click(function(e) {
+			if (lookbook_pic_number < lookbook.length) {
+				lookbook_pic_number += 1;
+				$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
+				$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
+			} else {
+				lookbook_pic_number = 1;
+				$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
+				$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
+			};
+		});
 
-	// 	$(".lb" + lookbook.number + "_left_arrow").click(function(e) {
-	// 		if (lookbook_pic_number > 1) {
-	// 			lookbook_pic_number -= 1;
-	// 			$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
-	// 			$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
-	// 		} else {
-	// 			lookbook_pic_number = lookbook.length;
-	// 			$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
-	// 			$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
-	// 		};
-	// 	});
-	// })
+		$(".lb" + lookbook.number + "_left_arrow").click(function(e) {
+			if (lookbook_pic_number > 1) {
+				lookbook_pic_number -= 1;
+				$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
+				$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
+			} else {
+				lookbook_pic_number = lookbook.length;
+				$("div.lb" + lookbook.number + "_active").removeClass("lb" + lookbook.number + "_active");
+				$("div.lookbook_" + lookbook_pic_number).addClass("lb" + lookbook.number + "_active");
+			};
+		});
+	});
 
 	//-------------------------------------
 	// Lookbook 3
 	//-------------------------------------
 
-	var lookbook_pic_number_1 = 1;
-	var lookbook_number_1 = 3;
-	var lookbook_length_1 = 9;
+	// var lookbook_pic_number_1 = 1;
+	// var lookbook_number_1 = 3;
+	// var lookbook_length_1 = 9;
 
-	$(".lookbook" + lookbook_number_1 + "_pic").click(function(e) {
-		var $target = e.target;
+	// $(".lookbook" + lookbook_number_1 + "_pic").click(function(e) {
+	// 	var $target = e.target;
 
-		if ($target.className == "lb" + lookbook_number_1 + "_right_arrow" || $target.className == "lb" + lookbook_number_1 + "_left_arrow") return;
+	// 	if ($target.className == "lb" + lookbook_number_1 + "_right_arrow" || $target.className == "lb" + lookbook_number_1 + "_left_arrow") return;
 
-		if (lookbook_pic_number_1 < lookbook_length_1) {
-			lookbook_pic_number_1 += 1;
-			$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
-		} else {
-			lookbook_pic_number_1 = 1;
-			$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
-		};
-	});
+	// 	if (lookbook_pic_number_1 < lookbook_length_1) {
+	// 		lookbook_pic_number_1 += 1;
+	// 		$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_1 = 1;
+	// 		$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
+	// 	};
+	// });
 
-	$(".lb" + lookbook_number_1 + "_right_arrow").click(function(e) {
-		if (lookbook_pic_number_1 < lookbook_length_1) {
-			lookbook_pic_number_1 += 1;
-			$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
-		} else {
-			lookbook_pic_number_1 = 1;
-			$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
-		};
-	});
+	// $(".lb" + lookbook_number_1 + "_right_arrow").click(function(e) {
+	// 	if (lookbook_pic_number_1 < lookbook_length_1) {
+	// 		lookbook_pic_number_1 += 1;
+	// 		$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_1 = 1;
+	// 		$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
+	// 	};
+	// });
 
-	$(".lb" + lookbook_number_1 + "_left_arrow").click(function(e) {
-		if (lookbook_pic_number_1 > 1) {
-			lookbook_pic_number_1 -= 1;
-			$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
-		} else {
-			lookbook_pic_number_1 = lookbook_length_1;
-			$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
-		};
-	});
+	// $(".lb" + lookbook_number_1 + "_left_arrow").click(function(e) {
+	// 	if (lookbook_pic_number_1 > 1) {
+	// 		lookbook_pic_number_1 -= 1;
+	// 		$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_1 = lookbook_length_1;
+	// 		$("div.lb" + lookbook_number_1 + "_active").removeClass("lb" + lookbook_number_1 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_1).addClass("lb" + lookbook_number_1 + "_active");
+	// 	};
+	// });
 
 	//-------------------------------------
 	// Lookbook 4
 	//-------------------------------------
 
-	var lookbook_pic_number_2 = 1;
-	var lookbook_number_2 = 4;
-	var lookbook_length_2 = 16;
+	// var lookbook_pic_number_2 = 1;
+	// var lookbook_number_2 = 4;
+	// var lookbook_length_2 = 16;
 
-	$(".lookbook" + lookbook_number_2 + "_pic").click(function(e) {
-		var $target = e.target;
+	// $(".lookbook" + lookbook_number_2 + "_pic").click(function(e) {
+	// 	var $target = e.target;
 
-		if ($target.className == "lb" + lookbook_number_2 + "_right_arrow" || $target.className == "lb" + lookbook_number_2 + "_left_arrow") return;
+	// 	if ($target.className == "lb" + lookbook_number_2 + "_right_arrow" || $target.className == "lb" + lookbook_number_2 + "_left_arrow") return;
 
-		if (lookbook_pic_number_2 < lookbook_length_2) {
-			lookbook_pic_number_2 += 1;
-			$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
-		} else {
-			lookbook_pic_number_2 = 1;
-			$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
-		};
-	});
+	// 	if (lookbook_pic_number_2 < lookbook_length_2) {
+	// 		lookbook_pic_number_2 += 1;
+	// 		$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_2 = 1;
+	// 		$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
+	// 	};
+	// });
 
-	$(".lb" + lookbook_number_2 + "_right_arrow").click(function(e) {
-		if (lookbook_pic_number_2 < lookbook_length_2) {
-			lookbook_pic_number_2 += 1;
-			$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
-		} else {
-			lookbook_pic_number_2 = 1;
-			$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
-		};
-	});
+	// $(".lb" + lookbook_number_2 + "_right_arrow").click(function(e) {
+	// 	if (lookbook_pic_number_2 < lookbook_length_2) {
+	// 		lookbook_pic_number_2 += 1;
+	// 		$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_2 = 1;
+	// 		$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
+	// 	};
+	// });
 
-	$(".lb" + lookbook_number_2 + "_left_arrow").click(function(e) {
-		if (lookbook_pic_number_2 > 1) {
-			lookbook_pic_number_2 -= 1;
-			$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
-		} else {
-			lookbook_pic_number_2 = lookbook_length_2;
-			$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
-		};
-	});
+	// $(".lb" + lookbook_number_2 + "_left_arrow").click(function(e) {
+	// 	if (lookbook_pic_number_2 > 1) {
+	// 		lookbook_pic_number_2 -= 1;
+	// 		$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_2 = lookbook_length_2;
+	// 		$("div.lb" + lookbook_number_2 + "_active").removeClass("lb" + lookbook_number_2 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_2).addClass("lb" + lookbook_number_2 + "_active");
+	// 	};
+	// });
 
 	//-------------------------------------
 	// Lookbook 5
 	//-------------------------------------
 
-	var lookbook_pic_number_3 = 1;
-	var lookbook_number_3 = 5;
-	var lookbook_length_3 = 6;
+	// var lookbook_pic_number_3 = 1;
+	// var lookbook_number_3 = 5;
+	// var lookbook_length_3 = 6;
 
-	$(".lookbook" + lookbook_number_3 + "_pic").click(function(e) {
-		var $target = e.target;
+	// $(".lookbook" + lookbook_number_3 + "_pic").click(function(e) {
+	// 	var $target = e.target;
 
-		if ($target.className == "lb" + lookbook_number_3 + "_right_arrow" || $target.className == "lb" + lookbook_number_3 + "_left_arrow") return;
+	// 	if ($target.className == "lb" + lookbook_number_3 + "_right_arrow" || $target.className == "lb" + lookbook_number_3 + "_left_arrow") return;
 
-		if (lookbook_pic_number_3 < lookbook_length_3) {
-			lookbook_pic_number_3 += 1;
-			$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
-		} else {
-			lookbook_pic_number_3 = 1;
-			$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
-		};
-	});
+	// 	if (lookbook_pic_number_3 < lookbook_length_3) {
+	// 		lookbook_pic_number_3 += 1;
+	// 		$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_3 = 1;
+	// 		$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
+	// 	};
+	// });
 
-	$(".lb" + lookbook_number_3 + "_right_arrow").click(function(e) {
-		if (lookbook_pic_number_3 < lookbook_length_3) {
-			lookbook_pic_number_3 += 1;
-			$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
-		} else {
-			lookbook_pic_number_3 = 1;
-			$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
-		};
-	});
+	// $(".lb" + lookbook_number_3 + "_right_arrow").click(function(e) {
+	// 	if (lookbook_pic_number_3 < lookbook_length_3) {
+	// 		lookbook_pic_number_3 += 1;
+	// 		$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_3 = 1;
+	// 		$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
+	// 	};
+	// });
 
-	$(".lb" + lookbook_number_3 + "_left_arrow").click(function(e) {
-		if (lookbook_pic_number_3 > 1) {
-			lookbook_pic_number_3 -= 1;
-			$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
-		} else {
-			lookbook_pic_number_3 = lookbook_length_3;
-			$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
-			$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
-		};
-	});
+	// $(".lb" + lookbook_number_3 + "_left_arrow").click(function(e) {
+	// 	if (lookbook_pic_number_3 > 1) {
+	// 		lookbook_pic_number_3 -= 1;
+	// 		$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
+	// 	} else {
+	// 		lookbook_pic_number_3 = lookbook_length_3;
+	// 		$("div.lb" + lookbook_number_3 + "_active").removeClass("lb" + lookbook_number_3 + "_active");
+	// 		$("div.lookbook_" + lookbook_pic_number_3).addClass("lb" + lookbook_number_3 + "_active");
+	// 	};
+	// });
 
 	//-------------------------------------
 	// Sale Modal - uncomment as needed
